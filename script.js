fdc2e3f4f04e1e03a11df88eec5e9a0c7966cb31
Michele Lugo <peachyjello@gmail.com>
2017-04-16 22:51:34 -0400

    2nd commit

--- script.js
+++ script.js
@@ -3,17 +3,118 @@ var ctx = undefined;
 var offset = 0.5;
 var jump = 10;
 
+var inputForm = document.querySelector('form');
+var options = document.getElementsByName('fun');
+var title = document.getElementsByName('title');
+var xax = document.getElementsByName('xax');
+var yax = document.getElementsByName('yax')
+
 function init(){
 	//initalizes the canvas
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
+	graph();
 }
 
 document.addEventListener('DOMContentLoaded',init);
 
 function graph(){
+	ctx.fillStyle = "white";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+
+	if(jump<0) return;
+
+	ctx.beginPath();
+	ctx.strokeStyle="#9ef2ff";
+	ctx.lineWidth =1;
+
+	for (var x = offset; x <= canvas.width; x+= jump) {
+		ctx.moveTo(x,0);
+		ctx.lineTo(x, canvas.height);
+	}
+
+	ctx.moveTo(x,0);
+	ctx.lineTo(x,canvas.height);
+
+	for (var y = offset; y <= canvas.height; y+= jump) {
+		ctx.moveTo(0,y);
+		ctx.lineTo(canvas.width,y);
+	}
+	ctx.stroke();
+
+	ctx.beginPath();
+	//x axis
+	ctx.moveTo(0,(canvas.height+jump)/2-offset);
+	ctx.lineTo(canvas.width,(canvas.height+jump)/2-offset);
+
+	// y axis
+	ctx.moveTo(canvas.width/2,-offset);
+	ctx.lineTo(canvas.width/2,canvas.height-offset);
+	ctx.strokeStyle="#999999";
+
+	ctx.stroke();
+}
+function draw() {
+	ctx.beginPath();
+	ctx.fillStyle="#999999";
+	ctx.font = "20px Helvetica";
+	ctx.fillText(title[0].value, 5, 20);
+	//draw a function
+	for (var i=0;i<options.length;i++){
+		if (options[i].value === "fun1" && options[i].checked) {
+			ctx.save();
+			fun1();
+			ctx.restore();
+			break;
+		}
+		else if (options[i].value === "fun2" && options[i].checked) {
+			fun2();
+			break;
+		}
+		else if (options[i].value === "fun3" && options[i].checked) {
+			fun3();
+			break;
+		}
+	}
+}
+
+function fun1(){
+	console.log('option 1');
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0000";
+
+	ctx.translate(canvas.width/2,canvas.height/2);
+	for (var x = -canvas.width/2; x <= canvas.width; x += jump) {
+        ctx.lineTo(x, eval(((.5 * x) + 4)*-1));
+    }
+	ctx.stroke();	
 
 }
+
+function fun2(){
+	console.log('option 2');
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0000";
+
+	ctx.translate(canvas.width/2,canvas.height/2);
+	for (var x = -canvas.width/2; x <= canvas.width; x += 2) {
+        ctx.lineTo(x, eval(-1*((x+2)*(x-4))));
+    }
+	ctx.stroke();	
+}
+
+function fun3(){
+	console.log('option 3');
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0000";
+
+	ctx.translate(canvas.width/2,canvas.height/2);
+	for (var x = -canvas.width/2; x <= canvas.width/2; x += jump) {
+        ctx.lineTo(x, -4);
+    }
+	ctx.stroke();	
+}
+
 
 /*
 
@@ -21,53 +122,7 @@ function update() {
 }
 
 function draw() {
-	canvasContext.fillStyle = "white";
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-	
-	if(jump<0)
-		return;
 
-    canvasContext.beginPath();
-	
-	// Draw Graph Paper.
-	canvasContext.strokeStyle="#00FFFF";
-	canvasContext.lineWidth=1;
-    
-    for (var x = offset; x <= canvas.width; x += jump) {
-
-        canvasContext.moveTo(x, 0);
-        canvasContext.lineTo(x, canvas.height);
-
-    }
-
-        canvasContext.moveTo(canvas.width-offset, 0);
-        canvasContext.lineTo(canvas.width-offset, canvas.height);
-        
-    for (var y = offset; y <= canvas.height; y += jump) {
-
-        canvasContext.moveTo(0, y);
-        canvasContext.lineTo(canvas.width, y);
-    
-    }
-    canvasContext.stroke();
-	
-	
-	// Setup Axises
-	canvasContext.strokeStyle="#000000";
-	canvasContext.lineWidth=2;
-		
-
-	canvasContext.beginPath();
-    // Draw X Axis
-	canvasContext.moveTo(0,(canvas.height+jump)/2-offset);
-    canvasContext.lineTo(canvas.width,(canvas.height+jump)/2-offset);
-	
-
-	// Draw Y Axis
-    canvasContext.moveTo(canvas.width/2,-offset);
-    canvasContext.lineTo(canvas.width/2,canvas.height-offset);
-
-    canvasContext.stroke();
 
 	// Draw Graph
 	canvasContext.save();

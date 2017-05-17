b74739f965a53309af923b482265859ef4f795f5
Michu Lugs <peachyjello@gmail.com>
2017-05-17 19:24:34 -0400

    more changes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var jump = 10;
 var inputForm = document.querySelector('form');
 var options = document.getElementsByName('fun');
 var title = document.getElementsByName('title');
+var choice = document.getElementById('choices');
 var xax = document.getElementsByName('xax');
 var yax = document.getElementsByName('yax')
 
@@ -62,22 +63,38 @@ function draw() {
 	ctx.fillText(xax[0].value,5, canvas.height/2-10);
 	ctx.fillText(yax[0].value,canvas.width/2+5,20);
 	//draw a function
+	ctx.save();
 	for (var i=0;i<options.length;i++){
 		if (options[i].value === "fun1" && options[i].checked) {
-			ctx.save();
+			
 			fun1();
-			ctx.restore();
-			break;
+			
 		}
 		else if (options[i].value === "fun2" && options[i].checked) {
 			fun2();
-			break;
 		}
 		else if (options[i].value === "fun3" && options[i].checked) {
 			fun3();
-			break;
+		}
+		else if (options[i].value === "selfun" && options[i].checked){
+			if (choices.options[choices.selectedIndex].value === "val1") {
+				fun4();
+			}
+			else if (choices.options[choices.selectedIndex].value === "val2"){
+				console.log("nothing happens.");
+				//fun5();
+			}
+			else if (choices.options[choices.selectedIndex].value === "val3"){
+				fun6();
+			}
+		}
+		else if (options[i].value === "manfun" && options[i].checked){
+			//manual function do
+			fun7();
 		}
 	}
+	ctx.restore();
+	i=0;
 }
 
 function fun1(){
@@ -116,7 +133,44 @@ function fun3(){
     }
 	ctx.stroke();	
 }
+var myconst = document.getElementById("myconst");
+var myconstval;
+myconst.addEventListener("change", function(){myconstval = myconst.value;},false);
 
+function fun4(){
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0000";
+	myconstval = parseInt(myconst.value);
+
+	ctx.translate(canvas.width/2,canvas.height/2);
+	for (var x = -canvas.width/2; x <= canvas.width/2; x += jump) {
+        ctx.lineTo(x, eval(myconst*Math.sin(2*x)));
+    }
+	ctx.stroke();	
+	console.log("Graphed sin function.")
+}
+
+function fun5(){
+	//Logarithmic function
+}
+
+function fun6(){
+	//console.log('csin(x)');
+	ctx.beginPath();
+	ctx.strokeStyle = "#FF0000";
+	myconstval = parseInt(myconst.value);
+
+	ctx.translate(canvas.width/2,canvas.height/2);
+	for (var x = -canvas.width/2; x <= canvas.width/2; x += jump) {
+        ctx.lineTo(x, Math.pow(x,myconstval));
+    }
+	ctx.stroke();	
+	console.log("Graphed exponential function.")
+}
+
+function fun7(){
+	// take in string value and interpret
+}
 
 /*
 
